650615c9d5248548ab1961745e263f26f427693c
Rajitha-Kilaru <krajitha061@gmail.com>
2024-02-27 18:17:52 +0530

    file-colocation and private route

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+function BarChart() {
+    return(
+        <h1>Bar Chart</h1>
+    )
+}
+
+export default function Dashboard() {
+    return (
+        <>
+        <BarChart />
+        <h1>Dashboard Page</h1>
+        </>
+    )
+}
