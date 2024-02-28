21b4db8f83ec22d0ee90b760c732c717bab00f36
Rajitha-Kilaru <krajitha061@gmail.com>
2024-02-28 18:02:13 +0530

    routing-metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: 'About my course',
+  }
+
 export default function About() {
     return (
         <h1>About Me</h1>
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,19 @@
+import { Metadata } from "next";
+
 interface ProductDetailsProps {
     params : {
         productId: string;
+    }
+}
+
+export const generateMetadata = async({ params }: ProductDetailsProps): Promise<Metadata> => {
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`android ${params.productId}`)
+        }, 1000)
+    })
+    return {
+        title : `product ${title}`
     }
 }
 
