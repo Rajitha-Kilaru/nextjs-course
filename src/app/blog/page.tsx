d77b3f3eec74e055749320e777ecfb44b85594aa
Rajitha-Kilaru <krajitha061@gmail.com>
2024-02-28 18:39:35 +0530

    title-metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata = {
+    title: {
+        absolute: 'Blog'
+    }
+}
+
 export default function Blog() {
     return (
         <h1>Blog Page</h1>
