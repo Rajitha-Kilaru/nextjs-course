df7dae02bc438401d5864504440eb3db5c53605e
Rajitha-Kilaru <krajitha061@gmail.com>
2024-02-28 12:38:06 +0530

    layout-header-footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style={{ backgroundColor: "lightblue", padding: '1rem' }}>Header</header>
+        {children}
+        <footer style={{ backgroundColor: "ghostwhite", padding: '1rem' }}>Footer</footer>
+      </body>
     </html>
   )
 }
