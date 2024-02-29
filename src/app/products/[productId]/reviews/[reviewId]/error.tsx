366560b2ccda7b10ffe704cad8491f91f97e3711
Rajitha-Kilaru <krajitha061@gmail.com>
2024-02-29 17:49:56 +0530

    recovering-from-errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,7 +1,10 @@
 "use client";
-export default function ErrorBoundary({error}: {error: Error}) {
+export default function ErrorBoundary({error, reset}: {error: Error, reset: () => void}) {
     return(
+        <>
         <h1>{error.message}</h1>
+        <button onClick={reset}>Try Again</button>
+        </>
     )
 }
 
