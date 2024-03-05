ce8376f493c520913286897cfaa5b174d54879f0
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-05 17:48:36 +0530

    unmatched-routes

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,7 +1,11 @@
 import Card from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
     return(
-        <Card>Notifications</Card>
+        <Card>
+            <div>Notifications</div>
+            <Link href="/complex-dashboard/archived">Archived</Link>
+            </Card>
     )
 }
---
+++ src/app/complex-dashboard/@revenue/default.tsx
@@ -0,0 +1,7 @@
+import Card from "@/components/card";
+
+export default function DefaultRevenueMetrics() {
+    return(
+        <Card>Default Revenue Metrics</Card>
+    )
+}
