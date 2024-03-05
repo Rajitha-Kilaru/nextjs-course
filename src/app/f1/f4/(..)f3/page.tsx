32023ecd281b94949a3ac68db0ff2a8048577af0
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-05 20:35:30 +0530

    intercepting-routees

---
+++ src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,7 @@
+export default function InterceptedF3() {
+    return (
+        <>
+        <h1>(..) intercepted F3 page</h1>
+        </>
+    )
+}
---
+++ src/app/f1/f4/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function F4() {
+    return (
+        <>
+        <h1>F4 page</h1>
+        <Link href="/f1/f3">F3 page</Link>
+        <Link href="/about">About</Link>
+        </>
+    )
+}
