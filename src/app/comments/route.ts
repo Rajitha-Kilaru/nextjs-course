d6556b7f4917a072182cecd970c8a602636dce6c
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-06 19:33:25 +0530

    url-query-params

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -1,23 +1,12 @@
+import { NextRequest } from 'next/server';
 import { comments } from './data'
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request : NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get("query");
+    const filteredComments = query ? comments.filter((item) => item.text.includes(query)) : comments;
+    return Response.json(filteredComments);
 }
-
-// export async function POST(request: Request) {
-//     const comment = await request.json();
-//     const newcomment = {
-//         id: comments.length + 1,
-//         text: comment.text
-//     }
-//     comments.push(newcomment);
-//     return new Response(JSON.stringify(newcomment), {
-//         headers: {
-//             "content-Type": "application/json",
-//         },
-//         status: 201,
-//     })
-// }
 
 export async function POST(request: Request) {
     try {
