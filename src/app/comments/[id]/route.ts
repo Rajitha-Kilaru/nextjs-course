963d3dcad64ef2cc3f893aeb1533ea0e5399e153
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-06 18:38:20 +0530

    handling-patch-request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -4,3 +4,11 @@ export async function GET(request: Request, { params }: { params: { id: string }
     const comment = comments.find((val) => val.id === parseInt(params.id))
     return Response.json(comment);
 }
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+    const body = request.json();
+    const { text } = body;
+    const index = comments.findIndex((val) => val.id === parseInt(params.id))
+    comments[index].text = "updated";
+    return Response.json(comments);
+}
