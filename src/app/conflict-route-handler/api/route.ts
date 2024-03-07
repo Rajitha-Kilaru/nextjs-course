f0ad93cfad23112681e6cf314c724fdd64ed9409
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-07 18:55:31 +0530

    cookies-in-route-handler

--- src/app/conflict-route-handler/api/route.ts
+++ src/app/conflict-route-handler/api/route.ts
@@ -1,17 +1,23 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     const headersList = headers();
 
+    cookies().set("resultperpage", "20")
+    const theme = request.cookies.get("theme")
+
     console.log(requestHeaders.get("Authorization"))
     console.log(headersList.get("Authorization"));
+    console.log(theme);
+    console.log(cookies().get("resultperpage"))
     
 
     return new Response("<h1>Route Handler with api</h1>", {
         headers : {
             "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     })
 }
