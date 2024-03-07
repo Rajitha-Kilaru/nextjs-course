b7100ecbdb4f9f119590e8a4be3e9aad42c2b853
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-07 10:48:47 +0530

    redirect-route-handler

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,6 +1,10 @@
+import { redirect } from 'next/navigation';
 import { comments } from '../data'
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(_request: Request, { params }: { params: { id: string } }) {
+    if(parseInt(params.id) > comments.length) {
+        redirect('/comments')
+    }
     const comment = comments.find((val) => val.id === parseInt(params.id))
     return Response.json(comment);
 }
