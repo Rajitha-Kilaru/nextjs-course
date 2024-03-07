2f6ebcbc3ed2c72fa837f5371637093334debb32
Rajitha-Kilaru <krajitha061@gmail.com>
2024-03-07 18:10:10 +0530

    headers-in-route-handler

--- src/app/conflict-route-handler/api/route.ts
+++ src/app/conflict-route-handler/api/route.ts
@@ -1,3 +1,17 @@
-export async function GET() {
-    return new Response("Route Handler with api")
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headersList = headers();
+
+    console.log(requestHeaders.get("Authorization"))
+    console.log(headersList.get("Authorization"));
+    
+
+    return new Response("<h1>Route Handler with api</h1>", {
+        headers : {
+            "Content-Type": "text/html",
+        }
+    })
 }
